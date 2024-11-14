a80f94a24e1868ad25e7a386f9148fc3fbb5e4c9
GroM <jerome.caporossi@ledger.fr>
2024-11-14 13:09:47 +0100

    All addresses from Trusted Name descriptor are base58 encoded

--- src/handle_provide_trusted_info.h
+++ src/handle_provide_trusted_info.h
@@ -3,7 +3,9 @@
 
 void handle_provide_trusted_info(void);
 
-extern Pubkey g_trusted_token_account_owner_pubkey;
+#define MAX_ADDRESS_LENGTH 44
+
+extern uint8_t g_trusted_token_account_owner_pubkey[MAX_ADDRESS_LENGTH + 1];
 extern bool g_trusted_token_account_owner_pubkey_set;
 
 #endif  // TRUSTED_INFO_H_
